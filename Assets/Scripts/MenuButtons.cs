c9008a7189433ee69ebc98197f3f618b24ac0fe5
TechGamesPlus <76413681+TechGamesPlus@users.noreply.github.com>
2022-05-04 06:47:47 +0800

    Unity Changes
    - Renamed SampleScene
    - Added Basic Menu

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // public void Options()
+    // {
+        // load options
+    // }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
